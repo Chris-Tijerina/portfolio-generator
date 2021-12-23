13dad7025cab484b3c7f5ee644116e46526f838c
Christopher Tijerina <ctijerina@live.com>
2021-12-22 22:32:00 -0600

    created the generate html file script and made it into a module

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+var fs = require("fs");
+const generatePage = require("./src/page-template");
+
 const profileDataArgs = process.argv.slice (2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log (profileDataArr[i]);
-    } 
+const [name, github] = profileDataArgs;
 
-    console.log("=========================");
+fs.writeFile("index.html", generatePage (name, github), err => {
+  if (err) throw err;
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log("portfolio complete! Check out index.html to see the output!")
+})
